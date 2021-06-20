99ded50fc27f5e0441fb3a75cab674dcedbb6be3
Janaki <janakishreeja23@gmail.com>
2021-06-20 23:27:38 +0400

    style - progress list

--- src/pages/ProgressList/ProgressList.tsx
+++ src/pages/ProgressList/ProgressList.tsx
@@ -30,22 +30,20 @@ export const ProgressList = () => {
   }
   return (
     <div>
-      <div>
-        <div className="text-center text-3xl font-semibold text-primary-color">
-          Completed Quizzes
-        </div>
-        <div className="grid grid-flow-col grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1 md-grid-flow-row gap-10 m-12 mt-20 place-content-center">
-          {progressList?.map(({ _id, numberOfAttempts, quiz }) => {
-            return (
-              <ProgressListItem
-                key={_id}
-                _id={_id}
-                numberOfAttempts={numberOfAttempts}
-                quiz={quiz}
-              />
-            );
-          })}
-        </div>
+      <div className="text-center text-3xl font-semibold text-primary-color">
+        Completed Quizzes
+      </div>
+      <div className="grid grid-flow-col grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1 md-grid-flow-row gap-10 m-12 mt-20 place-content-center">
+        {progressList?.map(({ _id, numberOfAttempts, quiz }) => {
+          return (
+            <ProgressListItem
+              key={_id}
+              _id={_id}
+              numberOfAttempts={numberOfAttempts}
+              quiz={quiz}
+            />
+          );
+        })}
       </div>
     </div>
   );
